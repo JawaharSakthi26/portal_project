594bdc2f53ac9d70e9bc0f6db5796c1ac2ff2881
Jawahar <jawaharsakthi.v@arkinfotec.com>
2023-12-06 11:17:52 +0530

    layout changes in form

--- user/_partials/form.php
+++ user/_partials/form.php
@@ -1,8 +1,8 @@
 <div class="row page-titles">
-    <div class="col-md-5 align-self-center">
+    <div class="col-md-5">
         <h4 class="text-themecolor">User Management</h4>
     </div>
-    <div class="col-md-7 align-self-center text-end">
+    <div class="col-md-7 text-end">
         <div class="d-flex justify-content-end align-items-center">
             <ol class="breadcrumb justify-content-end">
                 <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
@@ -11,57 +11,44 @@
         </div>
     </div>
 </div>
+
 <div class="row">
-    <div class="col-12">
+    <div class="col-md-12">
         <div class="card">
             <div class="card-body">
                 <h4 class="card-title">User <?php echo $test == '1' ? 'Edit' : 'Create'; ?></h4>
                 <form>
                     <div class="row">
-                        <div class="col-md-6">
-                            <div class="mb-3">
-                                <label for="tb-fname">Name</label>
-                                <input type="text" class="form-control" placeholder="Enter Name" value="<?php echo $test == '1' ? 'Test' : ''; ?>">
-                            </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="form-label">Name</label>
+                            <input type="text" class="form-control" placeholder="Enter Name" value="<?php echo $test == '1' ? 'Test' : ''; ?>">
                         </div>
-                        <div class="col-md-6">
-                            <div class="mb-3">
-                                <label for="tb-email">Code</label>
-                                <input type="number" class="form-control" placeholder="Enter code" value="<?php echo $test == '1' ? '100' : ''; ?>">
-                            </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="form-label">Code</label>
+                            <input type="number" class="form-control" placeholder="Enter code" value="<?php echo $test == '1' ? '100' : ''; ?>">
                         </div>
-                        <div class="col-md-6">
-                            <div class="mb-3">
-                                <div class="form-group">
-                                    <label class="form-label">Role</label>
-                                    <select class="form-select col-12" id="inlineFormCustomSelect">
-                                        <option selected>Choose role</option>
-                                        <option value="1" <?php echo $test == '1' ? 'selected' : '' ?>>Dev</option>
-                                        <option value="2">Frontend</option>
-                                        <option value="3">Design</option>
-                                        <option value="4">Network</option>
-                                    </select>
-                                </div>
-                            </div>
+                        <div class="col-md-6 mb-3">
+                            <label class="form-label">Role</label>
+                            <select class="form-select" id="inlineFormCustomSelect">
+                                <option selected>Choose role</option>
+                                <option value="1" <?php echo $test == '1' ? 'selected' : '' ?>>Dev</option>
+                                <option value="2">Frontend</option>
+                                <option value="3">Design</option>
+                                <option value="4">Network</option>
+                            </select>
                         </div>
-                        <div class="col-md-6">
-                            <div class="mb-3">
-                                <div class="form-group">
-                                    <label class="form-label">Status</label>
-                                    <select class="form-select col-12" id="inlineFormCustomSelect">
-                                        <option selected>Choose status</option>
-                                        <option value="1">Removed</option>
-                                        <option value="2">Other Team</option>
-                                        <option value="3" <?php echo $test == '1' ? 'selected' : '' ?>>Available</option>
-                                    </select>
-                                </div>
-                            </div>
+                        <div class="col-md-6 mb-3">
+                            <label class="form-label">Status</label>
+                            <select class="form-select" id="inlineFormCustomSelect">
+                                <option selected>Choose status</option>
+                                <option value="1">Removed</option>
+                                <option value="2">Other Team</option>
+                                <option value="3" <?php echo $test == '1' ? 'selected' : '' ?>>Available</option>
+                            </select>
                         </div>
                         <div class="col-12">
-                            <div class="d-md-flex align-items-center mt-3">
-                                <div class="ms-auto mt-3 mt-md-0">
-                                    <button type="submit" class="btn btn-primary text-white">Submit</button>
-                                </div>
+                            <div class="text-end">
+                                <button type="submit" class="btn btn-primary">Submit</button>
                             </div>
                         </div>
                     </div>
