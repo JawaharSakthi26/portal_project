5e9ebcb80689f75c03e035b5f9bfdea5ca19e2d3
Jawahar <jawaharsakthi.v@arkinfotec.com>
2023-12-06 10:22:41 +0530

    removed unwanted js file

--- layout/layout1.php
+++ layout/layout1.php
@@ -88,7 +88,6 @@
     </div>
     <script src="../assets/js/jquery.min.js"></script>
     <script src="../assets/js/bootstrap.bundle.min.js"></script>
-    <script src="../assets/js/perfect-scrollbar.jquery.min.js"></script>
     <script src="../assets/js/waves.js"></script>
     <script src="../assets/js/sidebarmenu.js"></script>
     <script src="../assets/js/custom.min.js"></script>
