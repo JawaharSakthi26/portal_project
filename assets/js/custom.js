3005fb13ae909b1360dfd680572c674975c0fb5f
Jawahar <jawaharsakthi.v@arkinfotec.com>
2023-12-11 20:01:55 +0530

    Used common function to render plugins

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,3 +9,32 @@ function datatable(selector) {
 function switchery(selector) {
   new Switchery($(selector)[0], $(selector).data());
 }
+
+function formWizard(selector) {
+  $(selector).steps({
+    headerTag: "h6",
+    bodyTag: "section",
+    transitionEffect: "fade",
+    titleTemplate: '<span class="step">#index#</span> #title#',
+    labels: {
+      finish: "Submit",
+    },
+    onFinished: function (event, currentIndex) {
+      Swal.fire("Form Submitted!", "Your data have been saved!");
+    },
+  });
+}
+
+function texteditor(selector) {
+  tinymce.init({
+    selector: selector,
+    height: 250,
+  });
+}
+
+function datepicker(selector) {
+  $(selector).datepicker({
+    autoclose: true,
+    todayHighlight: true,
+  });
+}
