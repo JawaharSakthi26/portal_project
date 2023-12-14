7da80e153b0243609b92f86a8112ba35b50ab4b0
Jawahar <jawaharsakthi.v@arkinfotec.com>
2023-12-14 10:06:18 +0530

    Removed button trigger while opening members tab

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -37,10 +37,9 @@ function formWizard(selector) {
       if (currentIndex > 0) {
         $(".actions > ul > li:first-child").attr("style", "");
         $(".actions > ul > li:nth-child(2)").attr("style", "margin-left:10px");
-      } 
-
-      if (selector == "#project-create" && currentIndex == "3") {
-        $(".add-user").trigger("click");
+      } else {
+        $(".actions > ul > li:first-child").attr("style", "display:none");
+        $(".actions > ul > li:nth-child(2)").attr("style", "margin-left:0px");
       }
     },
     onFinished: function (event, currentIndex) {
