b6d7371a21b0adbe718af947713df0c2d2721c98
Jawahar <jawaharsakthi.v@arkinfotec.com>
2023-12-15 10:14:09 +0530

    Removed unwanted from the project view code

--- project/view.php
+++ project/view.php
@@ -18,34 +18,34 @@
                 <h4 class="m-b-0 text-white">General Info</h4>
             </div>
             <div class="card-body">
-                <label for="" class="form-label fw-normal">Project Name</label>
+                <label class="form-label fw-normal">Project Name</label>
                 <p>Project Portal</p>
                 <hr>
-                <label for="" class="form-label fw-normal">Technologies</label>
+                <label class="form-label fw-normal">Technologies</label>
                 <p>React, Laravel, MySQl </p>
                 <hr>
-                <label for="" class="form-label fw-normal">Description</label>
+                <label class="form-label fw-normal">Description</label>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, sed modi! In dignissimos nam quidem? In sed architecto itaque aspernatur nobis soluta ad amet quos! Illum nobis dolor corporis dicta!</p>
                 <hr>
-                <label for="" class="form-label fw-normal">Development</label>
+                <label class="form-label fw-normal">Development</label>
                 <p>Scratch</p>
                 <hr>
-                <label for="" class="form-label fw-normal">Git Ownership</label>
+                <label class="form-label fw-normal">Git Ownership</label>
                 <p>WWU</p>
                 <hr>
-                <label for="" class="form-label fw-normal">Project Details Documentation</label>
+                <label class="form-label fw-normal">Project Details Documentation</label>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam, saepe doloribus eveniet rem repellat beatae tenetur libero! Omnis corporis, laborum minima aut consequatur possimus, sed itaque repellendus eveniet obcaecati atque repudiandae, cupiditate facere? Officia, nulla aspernatur voluptates, porro sit hic ut animi recusandae consectetur illo esse dolore cumque autem non.</p>
                 <hr>
-                <label for="" class="form-label fw-normal">Git URL</label>
+                <label class="form-label fw-normal">Git URL</label>
                 <p><code class="text-info">https://github.com/</code></p>
                 <hr>
-                <label for="" class="form-label fw-normal">Start Date</label>
+                <label class="form-label fw-normal">Start Date</label>
                 <p>12/12/2023</p>
                 <hr>
-                <label for="" class="form-label fw-normal">End Date</label>
+                <label class="form-label fw-normal">End Date</label>
                 <p>14/12/2023</p>
                 <hr>
-                <label for="" class="form-label fw-normal">Status</label>
+                <label class="form-label fw-normal">Status</label>
                 <p><span class="label label-success">Active</span></p>
             </div>
         </div>
@@ -57,19 +57,19 @@
                 <h4 class="m-b-0 text-white">Members</h4>
             </div>
             <div class="card-body">
-                <label for="" class="form-label fw-normal">Project Lead</label>
+                <label class="form-label fw-normal">Project Lead</label>
                 <p>User 1</p>
                 <hr>
-                <label for="" class="form-label fw-normal">Hierarchy Head</label>
+                <label class="form-label fw-normal">Hierarchy Head</label>
                 <p>User 2</p>
                 <hr>
-                <label for="" class="form-label fw-normal">Developer</label>
+                <label class="form-label fw-normal">Developer</label>
                 <p>User 3, User 4, User 5</p>
                 <hr>
-                <label for="" class="form-label fw-normal">Tester</label>
+                <label class="form-label fw-normal">Tester</label>
                 <p>User 6, User 7, User 8</p>
                 <hr>
-                <label for="" class="form-label fw-normal">Member</label>
+                <label class="form-label fw-normal">Member</label>
                 <p>User 9, User 10, User 11</p>
             </div>
         </div>
@@ -82,19 +82,19 @@
                 <h4 class="m-b-0 text-white">Live Details</h4>
             </div>
             <div class="card-body">
-                <label for="" class="form-label fw-normal">Credentials</label>
+                <label class="form-label fw-normal">Credentials</label>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt est itaque culpa earum dolore error repellat quis dicta, tempore quod?</p>
                 <hr>
-                <label for="" class="form-label fw-normal">Database</label>
+                <label class="form-label fw-normal">Database</label>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit commodi dolore id minus, dolorum facere eum quaerat et mollitia eveniet.</p>
                 <hr>
-                <label for="" class="form-label fw-normal">cPanel</label>
+                <label class="form-label fw-normal">cPanel</label>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In sapiente possimus recusandae dolore doloremque, unde eius iusto assumenda ducimus culpa!</p>
                 <hr>
-                <label for="" class="form-label fw-normal">WHM</label>
+                <label class="form-label fw-normal">WHM</label>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias hic minima nisi quasi magni doloremque tenetur ut amet consequuntur vel.</p>
                 <hr>
-                <label for="" class="form-label fw-normal">Others</label>
+                <label class="form-label fw-normal">Others</label>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam quibusdam veniam eos quisquam nobis dignissimos voluptatem qui doloremque asperiores minima!</p>
 
             </div>
@@ -106,19 +106,19 @@
                 <h4 class="m-b-0 text-white">Local Details</h4>
             </div>
             <div class="card-body">
-                <label for="" class="form-label fw-normal">Credentials</label>
+                <label class="form-label fw-normal">Credentials</label>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt est itaque culpa earum dolore error repellat quis dicta, tempore quod?</p>
                 <hr>
-                <label for="" class="form-label fw-normal">Database</label>
+                <label class="form-label fw-normal">Database</label>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit commodi dolore id minus, dolorum facere eum quaerat et mollitia eveniet.</p>
                 <hr>
-                <label for="" class="form-label fw-normal">cPanel</label>
+                <label class="form-label fw-normal">cPanel</label>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In sapiente possimus recusandae dolore doloremque, unde eius iusto assumenda ducimus culpa!</p>
                 <hr>
-                <label for="" class="form-label fw-normal">WHM</label>
+                <label class="form-label fw-normal">WHM</label>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias hic minima nisi quasi magni doloremque tenetur ut amet consequuntur vel.</p>
                 <hr>
-                <label for="" class="form-label fw-normal">Others</label>
+                <label class="form-label fw-normal">Others</label>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam quibusdam veniam eos quisquam nobis dignissimos voluptatem qui doloremque asperiores minima!</p>
             </div>
         </div>
