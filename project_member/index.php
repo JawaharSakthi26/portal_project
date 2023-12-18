bfe94dea98cdb1a9cad61248b0d6ce30bd6234d2
Jawahar <jawaharsakthi.v@arkinfotec.com>
2023-12-18 18:23:12 +0530

    Added more tables and changed styling in project members

--- project_member/index.php
+++ project_member/index.php
@@ -15,13 +15,14 @@
     <div class="col-lg-12">
         <div class="card">
             <div class="card-header bg-megna d-flex justify-content-between align-items-center">
-                <h4 class="m-0 text-white">Project Member's List</h4>
+                <h4 class="m-0 text-white">Active Projects</h4>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
                     <table id="project_members_list" class="display nowrap table table-hover table-striped border w-100">
                         <thead>
                             <tr>
+                                <th>#</th>
                                 <th>Projects</th>
                                 <th>Platform</th>
                                 <th>Development</th>
@@ -31,32 +32,100 @@
                         </thead>
                         <tbody>
                             <tr>
-                                <td>Project 1</td>
-                                <td>CI & Laravel Vue</td>
-                                <td>Scratch</td>
-                                <td>Tester 1</td>
-                                <td>Dev-1, Dev-2, Dev-3 (Dev-4, Dev-5)</td>
+                                <td></td>
+                                <td colspan="5" class="fw-bold fs-5">Regular</td>
                             </tr>
                             <tr>
-                                <td>Project 2</td>
-                                <td>PHP</td>
-                                <td>Existing</td>
-                                <td>Tester 1, Tester 2</td>
-                                <td>Dev-1, Dev-2 (Dev-3, Dev-4)</td>
+                                <td>1</td>
+                                <td>CheckIssuing</td>
+                                <td><span class="badge bg-dark">CI</span> <span class="badge bg-dark">Laravel</span> <span class="badge bg-dark">VUE</span></td>
+                                <td><span class="badge bg-warning">Existing</span></td>
+                                <td>Ajay</td>
+                                <td><span class="dedicated_dev">Kannan</span>, <span class="worked_dev">Athavan</span> (<span class="helpful_dev">Nadesh, Stanley</span>)</td>
                             </tr>
                             <tr>
-                                <td>Project 3</td>
-                                <td>Laravel Vue</td>
-                                <td>Existing</td>
-                                <td>Tester 2</td>
-                                <td>Dev-1, Dev-2, Dev-3, Dev-4 (Dev-5, Dev-6)</td>
+                                <td>2</td>
+                                <td>DentalSoft</td>
+                                <td><span class="badge bg-dark">PHP</span></td>
+                                <td><span class="badge bg-warning">Existing</span></td>
+                                <td>Ajay, Ahmad</td>
+                                <td><span class="project_head">Selva</span>, <span class="worked_dev">Kannan</span> (<span class="helpful_dev">Nantha, Nadesh</span>)</td>
                             </tr>
                             <tr>
-                                <td>Project 4</td>
-                                <td>Python Rasa</td>
-                                <td>Scratch</td>
-                                <td>Tester 1</td>
-                                <td>Dev-1, Dev-2, Dev-3(Dev-4, Dev-5)</td>
+                                <td>3</td>
+                                <td>TheNewMomSchool</td>
+                                <td><span class="badge bg-dark">Laravel</span> <span class="badge bg-dark">Vue</span></td>
+                                <td><span class="badge bg-info">Scratch</span></td>
+                                <td>Ahmad</td>
+                                <td><span class="project_head">Vasanth</span>, <span class="worked_dev">Vinoth, Athavan, Manoj</span> (<span class="helpful_dev">Nantha</span>)</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>10X Mile Value</td>
+                                <td><span class="badge bg-dark">Laravel</span></td>
+                                <td><span class="badge bg-warning">Existing</span></td>
+                                <td>Ajay, Ahmad</td>
+                                <td><span class="project_head">Nadesh</span>, <span class="worked_dev">Panneer, Hari, Kannan</span></td>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td colspan="5" class="fw-bold fs-5">Weekly</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>NYU</td>
+                                <td><span class="badge bg-dark">Laravel</span> <span class="badge bg-dark">Vue</span></td>
+                                <td><span class="badge bg-info">Scratch</span></td>
+                                <td>Ajay, Ahmad</td>
+                                <td><span class="project_head">Vinoth</span>,<span class="worked_dev"> Ranjith, Praba</span> (<span class="helpful_dev">Stanley, Vasanth</span>)</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>MzeroA</td>
+                                <td><span class="badge bg-dark">Angular</span> <span class="badge bg-dark">Laravel</span></td>
+                                <td><span class="badge bg-warning">Existing</span></td>
+                                <td>Ajay</td>
+                                <td><span class="project_head">Ranjith</span>,<span class="worked_dev"> Vinoth, Manoj, Athavan</span> (<span class="helpful_dev">Stanley, Vasanth</span>)</td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                                <td>Primo</td>
+                                <td><span class="badge bg-dark">Laravel</span></td>
+                                <td><span class="badge bg-info">Scratch</span></td>
+                                <td>Ajay</td>
+                                <td><span class="project_head">Nadesh</span>, <span class="worked_dev">Panneer, Hari, Kannan</span> (<span class="helpful_dev">Stanley</span>)</td>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td colspan="5" class="fw-bold fs-5">Hold / Some times</td>
+                            </tr>
+                            <tr>
+                                <td>8</td>
+                                <td>Beachify</td>
+                                <td><span class="badge bg-dark">React</span> <span class="badge bg-dark">Laravel</span></td>
+                                <td><span class="badge bg-info">Scratch</span></td>
+                                <td>Ahmad</td>
+                                <td><span class="project_head">Nantha</span>, <span class="worked_dev">Athavan, Vinoth, Selva</span></td>
+                            </tr>
+                            <tr>
+                                <td>9</td>
+                                <td>Driving</td>
+                                <td><span class="badge bg-dark">Yii2</span></td>
+                                <td><span class="badge bg-info">Scratch</span></td>
+                                <td>Ahmad</td>
+                                <td><span class="project_head">Vasanth</span>, <span class="worked_dev">Kannan, Ramesh</span></td>
+                            </tr>
+                            <tr>
+                                <td>10</td>
+                                <td>BPDS</td>
+                                <td><span class="badge bg-dark">CI</span></td>
+                                <td><span class="badge bg-warning">Existing</span></td>
+                                <td>Ahmad</td>
+                                <td><span class="project_head">Vasanth</span>, <span class="worked_dev">Kannan, Ramesh</span></td>
                             </tr>
                         </tbody>
                     </table>
@@ -66,13 +135,13 @@
     </div>
 </div>
 <div class="header">
-    <h4>Team Projects</h4>
+    <h4 class="">Team Projects</h4>
 </div>
 <div class="row">
-    <div class="col-6">
+    <div class="col-3">
         <div class="card">
             <div class="card-header bg-megna">
-                <h4 class="m-0 text-white">Team A[Stanley]</h4>
+                <h4 class="m-0 text-white fw-bold">Team A [Stanley]</h4>
             </div>
             <div class="card-body">
                 <div class="row">
@@ -99,10 +168,10 @@
             </div>
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-3">
         <div class="card">
             <div class="card-header bg-megna">
-                <h4 class="m-0 text-white">Team B[Nadesh]</h4>
+                <h4 class="m-0 text-white fw-bold">Team B [Nadesh]</h4>
             </div>
             <div class="card-body">
                 <div class="row">
@@ -129,12 +198,10 @@
             </div>
         </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-6">
+    <div class="col-3">
         <div class="card">
             <div class="card-header bg-megna">
-                <h4 class="m-0 text-white">Team C[Nantha]</h4>
+                <h4 class="m-0 text-white fw-bold">Team C [Nantha]</h4>
             </div>
             <div class="card-body">
                 <div class="row">
@@ -161,15 +228,15 @@
             </div>
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-3">
         <div class="card">
             <div class="card-header bg-megna">
-                <h4 class="m-0 text-white">Team D[Athavan]</h4>
+                <h4 class="m-0 text-white fw-bold">Team D [Athavan]</h4>
             </div>
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-12">
-                        <label class="form-label fw-normal">NNewMomSchoolYU</label>
+                        <label class="form-label fw-normal">NewMomSchoolYU</label>
                         <p>Athavan</p>
                     </div>
                     <hr>
