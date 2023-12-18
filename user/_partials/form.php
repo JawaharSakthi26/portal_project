d6d62f542372bb608035aeddde7c59776ba7ed90
Jawahar <jawaharsakthi.v@arkinfotec.com>
2023-12-18 18:21:55 +0530

    Changed the form fields in user create page

--- user/_partials/form.php
+++ user/_partials/form.php
@@ -1,21 +1,35 @@
 <div class="form-body">
     <div class="row">
         <div class="col-md-6">
-            <div class="form-group">
-                <label class="form-label">Name</label>
-                <input type="text" class="form-control" name="name" placeholder="Enter Name">
+            <div class="form-group required">
+                <label class="form-label">First Name</label>
+                <input type="text" class="form-control" name="first_name" placeholder="Enter First Name">
             </div>
         </div>
         <div class="col-md-6">
-            <div class="form-group">
-                <label class="form-label">Code</label>
-                <input type="number" class="form-control" name="code" placeholder="Enter code">
+            <div class="form-group required">
+                <label class="form-label">Last Name</label>
+                <input type="text" class="form-control" name="last_name" placeholder="Enter Last Name">
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-md-6">
-            <div class="form-group mb-0">
+            <div class="form-group required">
+                <label class="form-label">Email</label>
+                <input type="email" class="form-control" name="email" placeholder="Enter Email">
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-group required">
+                <label class="form-label">Password</label>
+                <input type="password" class="form-control" name="password" placeholder="Enter Password">
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group required mb-0">
                 <label class="form-label">Role</label>
                 <select class="form-control form-select" id="role" name="role">
                     <option selected> -- Choose role -- </option>
